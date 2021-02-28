f1e35ad3664d07db73bbe835a41e198936a4ad1d
ivanfabriano99 <ivanfabriano99@mail.ugm.ac.id>
2021-02-28 21:51:36 +0700

    Landing page and content first update

---
+++ src/components/HeaderContent.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-container fluid class="main">
+    <v-row no-gutters class="main-row">
+      <v-col cols="12" sm="12" md="6" class="header">
+        <h1>Hidroponika</h1>
+        <p>Make Our Smart Hidroponic Garden</p>
+      </v-col>
+      <v-col md="6" class="description">
+        <v-carousel
+          class="carousel"
+          cycle
+          height="400"
+          hide-delimiter-background
+          show-arrows-on-hover
+        >
+          <v-carousel-item v-for="(slide, i) in slides" :key="i">
+            <v-sheet :color="colors[i]" height="100%">
+              <v-row class="fill-height" align="center" justify="center">
+                <div class="display-3">
+                  {{ slide }} Slide
+                  <p :caption="caption">{{ caption[i] }}</p>
+                </div>
+              </v-row>
+            </v-sheet>
+          </v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      colors: ["transparent", "transparent", "transparent"],
+      slides: ["First", "Second", "Third"],
+      caption: ["Get Simpler Method", "Second", "Third"],
+    };
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;600;700&display=swap");
+.main {
+  font-family: "Montserrat", sans-serif;
+  margin: 0;
+}
+.main-row {
+}
+.header {
+  margin-top: 15vmax;
+  text-align: center;
+  justify-content: center;
+}
+.header > h1 {
+  color: #fdffff;
+  font-weight: 600;
+  font-size: 4em;
+}
+.header > p {
+  color: #fdffff;
+  font-weight: 400;
+  margin-top: -10px;
+}
+.description {
+  margin-top: 15vmax;
+  text-align: center;
+  justify-content: center;
+}
+.carousel {
+}
+.display-3 {
+  color: black;
+}
+.display-4 {
+  color: black;
+}
+
+@media screen and (max-width: 600px) {
+  .header > h1 {
+    color: #fdffff;
+    font-weight: 600;
+    font-size: 2em;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="main">
+    <header-content />
+    <div class="content">
+      <v-container fluid class="content-container">
+        <v-row class="first-row">
+          <div class="first-caption">
+            <h2>Make your hidroponic garden undercontrol</h2>
+            <p>
+              Not have a lot of time to control your hidroponic garden? let’s
+              try us, you can monitoring your garden easily and everywhere
+            </p>
+          </div>
+        </v-row>
+        <v-row class="features-row">
+          <v-col class="features-col"
+            ><img src="../assets/realtime.png" alt="Realtime" />
+            <p>Realtime Data Monitoring</p></v-col
+          >
+          <v-col class="features-col"
+            ><img src="../assets/history.png" alt="History" />
+            <p>Data History Collecting</p></v-col
+          >
+          <v-col class="features-col"
+            ><img src="../assets/devices.png" alt="Devices" />
+            <p>Integrated Sensor Device</p></v-col
+          >
+        </v-row>
+        <v-row class="third-row">
+          <div class="third-caption">
+            <h2>Yes, we are smart hidroponic garden systems</h2>
+            <p>
+              Not have a lot of time to control your hidroponic garden? let’s
+              try us, you can monitoring your garden easily and everywhere
+            </p>
+          </div>
+        </v-row>
+      </v-container>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderContent from "@/components/HeaderContent.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderContent,
+  },
+};
+</script>
+<style>
+#app {
+  background-image: url("../assets/bg.png");
+  background-repeat: no-repeat;
+  background-size: 100vmax 40vmax;
+  background-position: top left;
+}
+
+.content {
+  margin-top: 17rem;
+}
+.first-row {
+  justify-content: center;
+}
+.first-row .first-caption {
+  margin-bottom: 30px;
+  width: 35rem;
+  height: auto;
+}
+.first-caption > h2 {
+  font-weight: 600;
+  font-size: 2rem;
+  margin-bottom: 7px;
+}
+.first-caption > p {
+  font-weight: 300;
+}
+.features-row {
+  margin-top: 17rem;
+  background-color: #269c34;
+  padding: 50px;
+  margin: 0;
+}
+.features-col > p {
+  color: #fdffff;
+  margin-top: 30px;
+  font-weight: 600;
+}
+.third-caption > h2 {
+  font-weight: 600;
+  font-size: 2rem;
+  margin-bottom: 7px;
+  margin-top:80px;
+}
+.third-caption > p {
+  font-weight: 300;
+}
+@media screen and (max-width: 600px) {
+  .features-col {
+    margin: 10px 10px;
+  }
+}
+</style>
